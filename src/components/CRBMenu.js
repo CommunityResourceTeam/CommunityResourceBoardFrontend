25f72e01c4d09599e830fc8479143967b2a4c4fe
WeiHua Wu <weihua.wu@digitalskillsforall>
2026-08-10 15:03:03 -0700

    Changed the default names of component function to file name

--- src/components/CRBMenu.js
+++ src/components/CRBMenu.js
@@ -23,7 +23,7 @@ const options = [
 
 const ITEM_HEIGHT = 48;
 
-export default function LongMenu() {
+export default function CRBMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
