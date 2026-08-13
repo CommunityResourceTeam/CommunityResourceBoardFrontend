c4a4f186ea11ea21dd511f79d5696671138cbba1
WeiHua Wu <weihua.wu@digitalskillsforall>
2026-08-13 11:29:51 -0700

    Added Labels to the pages and styled them

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,4 @@
+// MUI Imports
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
@@ -5,36 +6,53 @@ import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import Stack from "@mui/material/Stack";
 
+// CRB Imports
 import CRBEmailInput from "../components/CRBEmailInput";
 import CRBPasswordInput from "../components/CRBPasswordInput";
 
+// Stack Layout
+const StackItem = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#ED9C40',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: (theme.vars ?? theme).palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
+
 function LoginStack() {
   return (
-    <Box sx={{ width: '100%', bgcolor: 'lightblue', }}>
-      <Stack
+    <Box sx={{ height: '100vh', width: '100%', bgcolor: '#FFFEF0', display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+      <Box sx={{ height: '50%', width: '40%', border: 10, borderRadius: '2em', borderColor: '#ED9C40', bgcolor: '#FFFEF0', display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+        <Stack
             direction="column"
-            spacing={0.5}
+            spacing={2}
             sx={{
               justifyContent: "space-evenly",
               alignItems: "center",
+              color: '#ED9C40',
             }}
-      >
-        <CRBEmailInput /> 
-        <CRBPasswordInput /> 
-
-        <Button
-          variant="contained"
-          sx={{
-            fontSize: '100%',
-            ml: 10,
-            bgcolor: "pink",
-            "&:hover": { bgcolor: "red" }
-          }}
         >
-          Login
-        </Button>
+          <Typography variant='h2'>Login</Typography>
+          <StackItem> <CRBEmailInput /> </StackItem> 
+          <StackItem> <CRBPasswordInput /> </StackItem> 
 
-      </Stack>
+          <Button
+            variant="contained"
+            sx={{
+              fontSize: '100%',
+              ml: 10,
+              bgcolor: "#FAB2EA",
+              "&:hover": { bgcolor: "#ED9C40" }
+            }}
+          >
+            Login
+          </Button>
+
+        </Stack>
+      </Box>
     </Box>
   );
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,3 +1,4 @@
+// MUI Imports
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
@@ -6,10 +7,12 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from "@mui/material/Stack";
 
+// CRB Imports
 import CRBEmailInput from "../components/CRBEmailInput";
 import CRBZipcodeInput from '../components/CRBZipCodeInput';
 import CRBPasswordInput from "../components/CRBPasswordInput";
 
+// Sign Up Name Input Field
 function SignUpName() {
   return (
     <Box
@@ -27,6 +30,7 @@ function SignUpName() {
   );
 }
 
+// Sign Up Username Input Field
 function SignUpUsername() {
   return (
     <Box
@@ -44,36 +48,51 @@ function SignUpUsername() {
   );
 }
 
+// Stack Layout
+const StackItem = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#ED9C40',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: (theme.vars ?? theme).palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
+
 function SignUpStack() {
   return (
-    <Box sx={{ width: '100%', bgcolor: 'lightblue', }}>
-      <Stack
+    <Box sx={{ height: '100vh', width: '100%', bgcolor: '#FFFEF0', display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+      <Box sx={{ height: '75%', width: '40%', border: 10, borderRadius: '2em', borderColor: '#ED9C40', bgcolor: '#FFFEF0', display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+        <Stack 
             direction="column"
             spacing={0.5}
             sx={{
               justifyContent: "space-evenly",
               alignItems: "center",
+              color: '#ED9C40',
             }}
-      >
-        <SignUpName /> 
-        <SignUpUsername /> 
-        <CRBEmailInput /> 
-        <CRBPasswordInput /> 
-        <CRBZipcodeInput /> 
-
-        <Button
-          variant="contained"
-          sx={{
-            fontSize: '100%',
-            ml: 10,
-            bgcolor: "pink",
-            "&:hover": { bgcolor: "red" }
-          }}
         >
-          Sign Up
-        </Button>
+          <Typography variant='h2'>Sign Up</Typography>
+          <StackItem> <SignUpName /> </StackItem>
+          <StackItem> <SignUpUsername /> </StackItem>
+          <StackItem> <CRBEmailInput /> </StackItem>
+          <StackItem> <CRBPasswordInput /> </StackItem>
+          <StackItem> <CRBZipcodeInput /> </StackItem>
 
-      </Stack>
+          <Button
+            variant="contained"
+            sx={{
+              fontSize: '100%',
+              ml: 10,
+              bgcolor: "#FAB2EA",
+              "&:hover": { bgcolor: "#ED9C40" }
+            }}
+          >
+            Sign Up
+          </Button>
+        </Stack>
+      </Box>
     </Box>
   );
 }
